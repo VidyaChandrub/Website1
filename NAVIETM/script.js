68d37f99c33176117bc50e48922665a87e7a3bde
Vidya <vidyac266@gmail.com>
2024-01-15 21:34:29 +0530

    sidebar

--- NAVIETM/script.js
+++ NAVIETM/script.js
@@ -1,18 +1,29 @@
-window.onload = function(){
-  const sidebar = document.querySelector(".sidebar-logo");
-  const closeBtn = document.querySelector("#btn");
+// window.onload = function(){
+//   const sidebar = document.querySelector(".sidebar-logo");
+//   const closeBtn = document.querySelector("#btn");
 
 
-  closeBtn.addEventListener("click",function(){
-      sidebar.classList.toggle("open")
-      menuBtnChange()
-  })
+//   closeBtn.addEventListener("click",function(){
+//       sidebar.classList.toggle("open")
+//       menuBtnChange()
+//   })
 
-  function menuBtnChange(){
-      if(sidebar.classList.contains("open")){
-          closeBtn.classList.replace("fa-bars","fa-bars-alt-right")
-      }else{
-          closeBtn.classList.replace("fa-bars-alt-right","fa-bars")
-      }
-  }
-}
+//   function menuBtnChange(){
+//       if(sidebar.classList.contains("open")){
+//           closeBtn.classList.replace("fa-bars","fa-bars-alt-right")
+//       }else{
+//           closeBtn.classList.replace("fa-bars-alt-right","fa-bars")
+//       }
+//   }
+// }
+
+$(document).ready(function(){
+    $('.sidebar-dropdown-menu').slideUp('fast')
+
+    $('.sidebar-menu-item.has-dropdown > a, .sidebar-dropdown-menu-item.has-dropdown > a').click(function(e){
+        e.preventDefault()
+
+        $(this).next().slideToggle('fast')
+        $(this).parent().toggleClass('focused')
+    })
+})
