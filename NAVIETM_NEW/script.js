99bff60ca14d966b66566510487f3d54a290007b
Vidya <vidyac266@gmail.com>
2024-01-19 22:13:32 +0530

    stepper

--- NAVIETM_NEW/script.js
+++ NAVIETM_NEW/script.js
@@ -2,4 +2,9 @@ const humburger = document.querySelector('#toggle-btn');
 
 humburger.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("expand")
+
 })
+
+// let changeIcon = function(humburger){
+//     humburger.classList.toggle('ri-arrow-down-s-line');
+// }
