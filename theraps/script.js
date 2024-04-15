6189fe527e8d826c95f2ff7567b3cee7c0e647ad
Vidya <vidyac266@gmail.com>
2024-04-15 18:31:04 +0530

    okie

--- theraps/script.js
+++ theraps/script.js
@@ -3,9 +3,11 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var insurance = document.getElementById('insurance-list').value;
     var message = document.getElementById('message');
   
-    if (name.trim() === '' || email.trim() === '') {
+    if (name.trim() === '' || email.trim() === '' || phone.trim() === '' ) {
       message.innerHTML = 'Please fill out all fields.';
       return;
     }
@@ -14,3 +16,27 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     // If form is valid, you can submit it to a server or perform other actions
     message.innerHTML = 'Form submitted successfully!';
   });
+
+
+  document.querySelectorAll('.palceholder').forEach(function(placeholder) {
+    placeholder.addEventListener('click', function() {
+      this.nextElementSibling.focus();
+    });
+  });
+  
+  document.querySelectorAll('.form-control').forEach(function(input) {
+    input.addEventListener('focus', function() {
+      this.previousElementSibling.style.display = 'none';
+    });
+  
+    input.addEventListener('blur', function() {
+      var $this = this;
+      if ($this.value.length === 0)
+        $this.previousElementSibling.style.display = 'block';
+    });
+  });
+  
+  document.querySelectorAll('.form-control').forEach(function(input) {
+    input.dispatchEvent(new Event('blur'));
+  });
+  
